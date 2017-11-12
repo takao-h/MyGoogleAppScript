ec8704e42f5bd44083549bf8820652a00bdf4436
takao-h <hayashi_takao@cyberbuzz.co.jp>
2017-11-13 00:06:57 +0900

    Refactering

--- fetchAppointmentSchedule.js
+++ fetchAppointmentSchedule.js
@@ -3,12 +3,11 @@ function fetchAppointmentSchedules() {
   var EMAIL = 'hoge@gmail.com';
 
   // sheet name
-  var SHEET_NAME = 'sheet';
+  var SHEET_NAME = 'シート';
   // start clomun number
   var RANGE = 2;
   // time format
-  var FORMAT_TIME = 'yyyy:dd:hh:mm';
-
+  var FORMAT_TIME = 'yyyy/MM/dd';
   // get sheet info
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME).clear();
 
@@ -16,20 +15,19 @@ function fetchAppointmentSchedules() {
   var calender = CalendarApp.getCalendarById(EMAIL);
   // get Event
   var schedules = calender.getEventsForDay(new Date());
-  var count = 0;
-  var index = 0;
 
-  if (schedules[index].getTitle().match(/【あぽ】|【アポ】|【ｱﾎﾟ】|【apo】|【あポ】|【アぽ】/i)){
+  var count = 0;
+
+  for(var index = 0; index < schedules.length; index++) {
+    if (schedules[index].getTitle().match(/【あぽ】|【アポ】|【ｱﾎﾟ】|【apo】|【あポ】|【アぽ】/i)){
       count++;
+    }
   }
   var range = RANGE + index;
 
   // insert name
-  sheet.getRange(range, 1).setValue('your_name')
-
-
+  sheet.getRange(range, 1).setValue('Name')
   // Output matched condition
   sheet.getRange(range, 2).setValue(count);
-
 
 }
